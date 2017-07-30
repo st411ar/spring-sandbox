028de18b66c80f735d391f6f81572d42bff6a338
Vitaly Kiselev <vitaly.kiselev@axamit.com>
2017-07-31 01:59:04 +0300

    initial commit

---
+++ src/main/java/org/st411ar/spring/sandbox/SandboxApplication.java
@@ -0,0 +1,12 @@
+package org.st411ar.spring.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxApplication.class, args);
+	}
+}
